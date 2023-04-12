725285733ed6b55f15760f665c9b7fed719f446a
Valentine Bontemps <valveryne@hotmail.com>
2023-04-12 15:01:08 +0200

    Ajout de React Router + installation des dossiers

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../assets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          👻.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
