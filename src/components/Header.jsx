f5856d4b5d8a5032ad1ab5ad6c326da0bc7a4b3c
Valentine Bontemps <valveryne@hotmail.com>
2023-04-13 16:44:14 +0200

    Initialisation du CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
+import '../assets/Header.css'
 
 function Header() {
     return (
-        <div>
-            <h1>Patatipatata</h1>    
-            <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">À propos</Link>
+        <div className='kasa-header'>
+            <h1 className='kasa-title-header'>Patatipatata</h1>    
+            <nav className='kasa-nav-header'>
+                <Link to="/" className='kasa-links1-header'>Accueil</Link>
+                <Link to="/about" className='kasa-links2-header'>À propos</Link>
             </nav>
         </div>
         );
