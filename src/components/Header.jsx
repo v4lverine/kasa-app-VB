800a5f2b43861cc04d934065395c5ba7123e18e5
Valentine Bontemps <valveryne@hotmail.com>
2023-04-19 15:50:32 +0200

    Ajout alt logo du site dans Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className='kasa-header'>
             <h1 className='kasa-title-header'>Kasa</h1>
-            <img src='./images/kasa-logo.png' className='kasa-logo'/>    
+            <img src='./images/kasa-logo.png' className='kasa-logo' alt='logo de Kasa'/>    
             <nav className='kasa-nav-header'>
                 <Link to="/" className='kasa-links1-header'>Accueil</Link>
                 <Link to="/about" className='kasa-links2-header'>À propos</Link>
