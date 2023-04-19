63e14b2ce117ff31e4d300bfe436949a8948db28
Valentine Bontemps <valveryne@hotmail.com>
2023-04-19 15:45:43 +0200

    Page error ajoutée

--- src/pages/404/Error.jsx
+++ src/pages/404/Error.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+import "../../assets/Error.css"
+
 function Error() {
     return (
-        <div>
-            <h1>404</h1>    
+        <div className='kasa-404-block'>
+            <b className='kasa-404'>404</b>
+            <p className='kasa-404-oopsie'>Oups ! La page que vous demandez n'existe pas.</p>
+            <Link to={'/'} className='kasa-404-linkto'>Retourner à la page d'accueil</Link>    
         </div>
         );
     }
