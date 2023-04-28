9c741181071ab22c08aa77dc817583f94d08b793
Valentine Bontemps <valveryne@hotmail.com>
2023-04-28 14:40:10 +0200

    Logo n&b ajouté

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import '../assets/Footer.css'
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div className='footer'>
-            <h2 className='title-footer'>Kasa</h2>
+            <h2 className='title-footer'>K<img src='/images/logo-kasa.png' alt='logo noir et blanc de Kasa' />sa</h2>
             <p className='text-footer'>© 2020 Kasa. All rights reserved</p>
         </div>
     );
